dc92972402f7616381cc9714080de3e54e81e145
Evan <egunn93@protonmail.com>
2022-10-17 19:51:13 -0500

    Better implementation of item giving

--- src/main/java/art/tidsear/pumpkinstore/StoreSignBlockSign.java
+++ src/main/java/art/tidsear/pumpkinstore/StoreSignBlockSign.java
@@ -5,17 +5,28 @@ import art.tidsear.pumpkinking.PumpkinKingMod;
 import art.tidsear.pumpkinking.PumpkinKingModBlocks;
 import cpw.mods.fml.common.registry.GameRegistry;
 import net.minecraft.block.BlockSign;
+import net.minecraft.command.CommandGive;
+import net.minecraft.command.ICommandSender;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.tileentity.TileEntity;
 import net.minecraft.tileentity.TileEntitySign;
 import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.ChunkCoordinates;
+import net.minecraft.util.IChatComponent;
 import net.minecraft.world.World;
 import org.jetbrains.annotations.NotNull;
+import org.lwjgl.Sys;
 
 public class StoreSignBlockSign extends BlockSign {
+
+    private ICommandSender sender;
+    private CommandGive cmdGive;
     public StoreSignBlockSign(boolean onGround) {
         super(TileEntitySign.class, onGround);
+        cmdGive = new CommandGive();
+        sender = new StoreSender();
     }
 
     @Override
@@ -49,18 +60,11 @@ public class StoreSignBlockSign extends BlockSign {
 
         if (player.capabilities.isCreativeMode) {
             player.addChatMessage(new ChatComponentText("Cost was: "+cost));
-            giveItem(String.format("%s:%s",tes.signText[1],tes.signText[2]+tes.signText[3]), player);
+            cmdGive.processCommand(this.sender,new String[]{player.getDisplayName(), tes.signText[1]+":"+tes.signText[2]+tes.signText[3]});
         } else if (PumpkinKingMod.pkGameMode.getState() != PKState.IDLE) {
             // logic for checking / subtracting points from a player
         }
         return true;
-    }
-
-    private void giveItem(String itemName, EntityPlayer player) {
-        ItemStack stack = GameRegistry.makeItemStack(itemName,0,1,"");
-        if (stack != null) {
-            player.inventory.addItemStackToInventory(stack);
-        }
     }
 
     private int parseBuySign(String line) {
@@ -91,5 +95,39 @@ public class StoreSignBlockSign extends BlockSign {
             System.out.println("exception?");
             return -1;
         }
+    }
+}
+
+class StoreSender implements ICommandSender {
+
+    @Override
+    public String getCommandSenderName() {
+        return "Store";
+    }
+
+    @Override
+    public IChatComponent func_145748_c_() {
+        return new ChatComponentText(this.getCommandSenderName());
+    }
+
+    @Override
+    public void addChatMessage(IChatComponent p_145747_1_) {
+        System.out.println(p_145747_1_.getUnformattedText());
+    }
+
+    @Override
+    public boolean canCommandSenderUseCommand(int p_70003_1_, String p_70003_2_) {
+        System.out.println("HI");
+        return true;
+    }
+
+    @Override
+    public ChunkCoordinates getPlayerCoordinates() {
+        return null;
+    }
+
+    @Override
+    public World getEntityWorld() {
+        return null;
     }
 }
